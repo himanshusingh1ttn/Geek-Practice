110b8e922979f5b35c2ea32c5e027968e7fa9544
bhaskarfulara <bhaskar.fulara@tothenew.com>
2022-05-03 00:54:41 +0530

    Complete 8 video steps

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/userModel');
 
+const bcrypt=require('bcrypt')
+const securePassword=async(password)=>{
+    try {
+        const passwordHash=await bcrypt.hash(password,10);
+        return passwordHash;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 const loadRegister = async (req,res)=>{
     try{
        res.render('registration');
@@ -9,4 +18,30 @@ const loadRegister = async (req,res)=>{
     }
 
 }
-module.exports = {loadRegister};
+
+const insertUser=async(req,res)=>{
+    try{
+        const spassword=await securePassword(req.body.password);
+        const user =new User({
+            name:req.body.name,
+            email:req.body.email,
+            mobile:req.body.mno,
+            image:req.file.filename,
+            password:spassword,
+            is_admin:0
+        });
+        const userData=await user.save();
+        if(userData){
+            res.render('registration',{message:"Your registration done.Please verify your mail."})
+        }
+        else{
+            res.render('registration',{message:"Your registration failed."})
+        }
+    }
+    catch(error){
+        console.log(error.message);
+    }
+}
+
+
+module.exports = {loadRegister,insertUser};
